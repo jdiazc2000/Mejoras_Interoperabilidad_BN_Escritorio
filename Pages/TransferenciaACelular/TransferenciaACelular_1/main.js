af2de2d60bfe9e5ad26c675b59fc6d9449a3d054
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2024-03-08 00:32:08 -0500

    Five commit

--- Pages/TransferenciaACelular/TransferenciaACelular_1/main.js
+++ Pages/TransferenciaACelular/TransferenciaACelular_1/main.js
@@ -1,79 +1,100 @@
-$(document).ready(function(){
-    Off();
-    console.log(Entidades[0].nombre)
-});
-
 let IsRadioSelected = false;
 let IsCodeFull = false;
+let ultimoCheckboxSeleccionado = "";
 
-$("#Celular").on("input", function() {  
-    if($("#Celular").val().length == 9){
-           IsCodeFull = true;
-    }else{
-           IsCodeFull = false;
+$(document).ready(function () {
+  Off();
+
+  $("#BN").change(function () {
+    if ($(this).is(":checked")) {
+        ocultarTodosLosElementosP()
     }
-
-    if(IsCodeFull && IsRadioSelected){
-           On()
-    }else{
-           Off()
-    }
-
-    console.log(IsCodeFull)  
-});
-
-
-$('input[type="radio"]').change(function() {
-    if($('input[name="ritem"]:checked').val() !== null){
-        var valorSeleccionado = $('input[name="ritem"]:checked').val();
-        console.log("Valor seleccionado: " + valorSeleccionado);
-        IsRadioSelected = true
-    }else{
-        console.log("no")
-        IsRadioSelected = false
-    }
-
-        
-    if(IsCodeFull && IsRadioSelected){
-        On()
-    }else{
-        Off()
- }
-});
-
-function On(){
-    $(".btn2").html("CONTINUAR"+"<img src=" + "../../../assets/VECTOR-ICON-MV/Vector.svg" + ">")
-    $(".btn2").prop("disabled",false)
-    $(".btn2").css({"color": "rgba(255, 255, 255, 1)"})
-    $(".btn2").css({"background-color": "rgba(197, 20, 22, 1)"})
-    $(".btn2").css({"border-color": "rgba(197, 20, 22, 1)"})
-    $(".btn2").css({"cursor": "pointer"})
-}
-
-function Off(){
-    $(".btn2").html("CONTINUAR"+"<img src=" + "../../../assets/VECTOR-ICON-MV/Vector-1.svg" + ">")
-    $(".btn2").prop("disabled",true)
-    $(".btn2").css({"color": "rgba(79, 79, 79, 1)"})
-    $(".btn2").css({"background-color": "rgba(215, 215, 215, 1)"})
-    $(".btn2").css({"border-color": "rgba(215, 215, 215, 1)"})
-    $(".btn2").css({"cursor": "default"})
-}
-
-$(".btn1").click(function(){
-    document.location.href="../../TransferenciaACelular/TransferenciaACelular_0"
-})
-
-$(".btn2").click(function(){
-    //document.location.href="../../TransferenciaACelular/TransferenciaACelular_0"
-})
-
-
-let Entidades = [
-    { nombre: "Plin", logourl: "../../../assets/VECTOR-ICON-MV/plinlogo.svg" },
-    { nombre: "Yape", apellido: "../../../assets/VECTOR-ICON-MV/yapelogo.svg" },
-    { nombre: "Caja Arequipa", apellido: "../../../assets/VECTOR-ICON-MV/cajaarequipalogo.svg" },
-    { nombre: "Banco Pichincha", apellido: "../../../assets/VECTOR-ICON-MV/bancopichinchalogo.svg" },
-];
+  });
   
+  $('.EntidadSquare input[type="checkbox"]').change(function () {
+    var targetId = $(this).data("target");
+    if ($('.EntidadSquare input[type="checkbox"]:checked').length === 0) {
+      ocultarTodosLosElementosP();
+    } else {
+      ocultarTodosLosElementosP();
+      $("#" + targetId).css("visibility", "visible");
+    }
+  });
+});
 
+$(document).on("click", 'input[type="checkbox"]', function () {
+  $('input[type="checkbox"]').not(this).prop("checked", false);
+});
 
+$("#monto").on("input", function () {
+  if ($("#monto").val() <= 500.00) {
+    IsCodeFull = true;
+  } else {
+    IsCodeFull = false;
+  }
+
+  console.log(IsCodeFull);
+
+  if (IsCodeFull && IsRadioSelected) {
+    On();
+  } else {
+    Off();
+  }
+});
+
+$('input[type="checkbox"]').change(function () {
+  if ($('input[name="ritem"]:checked').val() !== undefined) {
+    console.log($('input[name="ritem"]:checked').val())
+    IsRadioSelected = true;
+  } else {
+    console.log("no");
+    IsRadioSelected = false;
+  }
+
+  if (IsCodeFull && IsRadioSelected) {
+    On();
+  } else {
+    Off();
+  }
+});
+
+function ocultarTodosLosElementosP() {
+    $(".EntidadSquare p").css("visibility", "hidden");
+}
+
+function On() {
+  $(".btn2").html(
+    "CONTINUAR" +
+      "<img src=" +
+      "../../../assets/VECTOR-ICON-MV/Vector.svg" +
+      ">"
+  );
+  $(".btn2").prop("disabled", false);
+  $(".btn2").css({ color: "rgba(255, 255, 255, 1)" });
+  $(".btn2").css({ "background-color": "rgba(197, 20, 22, 1)" });
+  $(".btn2").css({ "border-color": "rgba(197, 20, 22, 1)" });
+  $(".btn2").css({ cursor: "pointer" });
+}
+
+function Off() {
+  $(".btn2").html(
+    "CONTINUAR" +
+      "<img src=" +
+      "../../../assets/VECTOR-ICON-MV/Vector-1.svg" +
+      ">"
+  );
+  $(".btn2").prop("disabled", true);
+  $(".btn2").css({ color: "rgba(79, 79, 79, 1)" });
+  $(".btn2").css({ "background-color": "rgba(215, 215, 215, 1)" });
+  $(".btn2").css({ "border-color": "rgba(215, 215, 215, 1)" });
+  $(".btn2").css({ cursor: "default" });
+}
+
+$(".btn1").click(function () {
+  document.location.href =
+    "../../TransferenciaACelular/TransferenciaACelular_0";
+});
+
+$(".btn2").click(function () {
+  //document.location.href="../../TransferenciaACelular/TransferenciaACelular_0"
+});
